bd06de68bf1bcebcf24ad62e40a7cda2e1902ea3
Anderson Rivera <anderson.pr.dev@gmail.com>
2025-02-19 23:28:57 -0500

    upd User repo

--- src/main/java/com/anderson/users/repositories/UserRepository.java
+++ src/main/java/com/anderson/users/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.anderson.users.repositories;
 import com.anderson.users.models.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
