8dd8a1462bfc90016f18f8ae14d4a10f548e45cb
Anderson Rivera <anderson.pr.dev@gmail.com>
2025-02-19 23:27:22 -0500

    upd filter config files

--- src/main/java/com/anderson/users/auth/SpringSecurityConfig.java
+++ src/main/java/com/anderson/users/auth/SpringSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.anderson.users.auth;
 
 import com.anderson.users.auth.filters.JWTAuthFilter;
+import com.anderson.users.auth.filters.JWTValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,19 +23,32 @@ public class SpringSecurityConfig {
         this.authenticationConfiguration = authenticationConfiguration;
     }
 
+    // to decrypt/encrypt encrypted passwords
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // SECURITY RULES
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users").permitAll() // all req are permitted
+                        .requestMatchers(HttpMethod.GET, "/api/users/{id}").hasAnyRole("USER", "ADMIN") // all roles specified here can access to this url
+                        .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN") // Just ADMIN is permitted
+                        .requestMatchers("/api/users/**").hasRole("ADMIN") // any route after this method (delete, put, etc.) only ADMIN can access it.
+                        //.requestMatchers(HttpMethod.DELETE, "/api/users/{id}").hasRole("ADMIN") you also can define one by one like this.
+                        //.requestMatchers(HttpMethod.PUT, "/api/users/{id}").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilter(new JWTAuthFilter(authenticationConfiguration.getAuthenticationManager()))
+                .addFilter(new JWTValidationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf(config -> config.disable())
                 .sessionManagement(management -> management
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
